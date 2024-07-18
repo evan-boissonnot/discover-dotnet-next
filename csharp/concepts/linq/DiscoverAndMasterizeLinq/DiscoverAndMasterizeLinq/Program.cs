4a3d577b04ec9917e0b32c49081b147aa8d867c4
Evan BOISSONNOT <evan@boissonnot.fr>
2024-07-18 17:16:39 +0200

    feat: first step

---
+++ csharp/concepts/linq/DiscoverAndMasterizeLinq/DiscoverAndMasterizeLinq/Program.cs
@@ -0,0 +1,46 @@
+﻿List<string> ensembleParent = new() { "Sam", "Frodo", "Grand pas" };
+
+for (int i = 0; i < ensembleParent.Count; i++)
+{
+	Console.WriteLine(ensembleParent[i]);
+}
+
+foreach (string sdaCharacter in ensembleParent)
+{
+	Console.WriteLine(sdaCharacter);
+}
+
+
+Console.WriteLine("-----------------DIFFERE ------------------------");
+
+var request = from character in ensembleParent
+			  where character.Length > 3
+			  select character;
+
+foreach (string character in request)
+{
+	Console.WriteLine(character);
+}
+
+ensembleParent.Add("Gandalf");
+
+foreach (string character in request)
+{
+	Console.WriteLine(character);
+}
+
+Console.WriteLine("============ IMMEDIAT ===========");
+
+var resultRequest = request.ToList();
+foreach (string character in resultRequest)
+{
+	Console.WriteLine(character);
+}
+
+ensembleParent.Add("Legolas");
+
+foreach (string character in resultRequest)
+{
+	Console.WriteLine(character);
+}
+
