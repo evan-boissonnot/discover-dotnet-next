c98d4b5ba8d84928c8b2552bcaf960f431831e22
Evan BOISSONNOT <evan@boissonnot.fr>
2024-07-22 19:36:23 +0200

    feat: linq

---
+++ asp.net core/DiscoverAspNetCore9/dotnet9.linqs/Models.cs
@@ -0,0 +1,16 @@
+﻿namespace dotnet9.linqs
+{
+    internal class Weapon(string label, int power = 10)
+    {
+        public string Label { get; set; } = label;
+
+        public int Power { get; set; } = power;
+    }
+
+    internal class Jedi(string surname, Weapon weapon)
+    {
+        public string Surname { get; set; } = surname;
+
+        public Weapon Weapon { get; set; } = weapon;
+    }
+}
---
+++ asp.net core/DiscoverAspNetCore9/dotnet9.linqs/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+using dotnet9.linqs;
+
+List<Jedi> jedis = new() { new Jedi("Vador", new("Sabre laser", power: 15)), new Jedi("Leia", new("Taser")), new Jedi("Luke 2", new("Sabre laser")), new Jedi("Jar jar", new("PistoLaser", power: 20)) };
+
+var list = jedis.CountBy(item => item.Weapon.Label);
+foreach (var item in list)
+{
+    Console.WriteLine("{0} {1}", item.Key, item.Value);
+}
+
+Console.WriteLine("-".PadLeft(10, '='));
+
+var result = jedis.AggregateBy(keySelector: item => item.Weapon.Label, seed: 0, (result, jedi) => result + jedi.Weapon.Power);
+foreach (var item in result)
+{
+    Console.WriteLine("{0} {1}", item.Key, item.Value);
+}
+
+
+Console.WriteLine("-".PadLeft(10, '='));
+var resultIndexs = jedis.Index();
+foreach (var item in resultIndexs)
+{
+    Console.WriteLine("{0} {1}", item.Index, item.Item.Surname);
+}
